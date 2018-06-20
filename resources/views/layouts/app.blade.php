c294015724de41d525ed44976f7c66bf7b55f8f4
Hironari Iwata <hiro_wrty1028_java@yahoo.co.jp>
2018-06-20 10:58:40 +0900

    nav実装

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,14 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
     </head>
     <body>
+        <nav>
+            <div class="nav-wrapper">
+                <a href="{{ url('posts/') }}" class="brand-logo"> Sample APP </a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="{{ url('posts/create') }}">New Post</a></li>
+                </ul>
+            </div>
+        </nav>
         @yield('content')
         <!--JavaScript at end of body for optimized loading-->
         <script type="text/javascript" src="js/materialize.min.js"></script>

9c28e6b4e71252c5393d40f30892e75f73bbb9e3
Hironari Iwata <hiro_wrty1028_java@yahoo.co.jp>
2018-06-20 09:50:32 +0900

    Materialize 導入

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,8 +2,14 @@
 <html lang="ja">
     <head>
         <title>@yield('title')</title>
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
     </head>
     <body>
         @yield('content')
+        <!--JavaScript at end of body for optimized loading-->
+        <script type="text/javascript" src="js/materialize.min.js"></script>
     </body>
 </html>
